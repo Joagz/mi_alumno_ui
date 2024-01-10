19ae8dc577fb099d3c5bf8980f15f557b106f77e
joagz <joagomez.dev@gmail.com>
2024-01-10 11:50:49 -0300

    Changed the list to cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         </FeaturedLink>
       </FeaturedLinkContainer>
       <div className='px-4 w-full flex flex-col justify-center items-center my-24'>
-        <div className='xl:w-[60%] lg:w-3/4 md:w-3/2 w-full flex flex-col gap-6'>
+        <div className='xl:w-[60%] lg:w-3/4 md:w-3/2 w-full flex flex-col gap-24'>
           <div className='flex flex-col gap-2'>
             <h1 className='text-2xl font-bold'>
               Misión Institucional de MiAlumno.com
@@ -45,8 +45,7 @@ export default function Home() {
               educativo.
             </p>
           </div>
-          <h2 className='text-2xl font-bold'>Servicios Destacados:</h2>
-          <ListContainer>
+          {/* <ListContainer>
             <ContentListItem
               content='MiAlumno.com facilita el proceso de registro para las
                   escuelas, permitiéndoles crear perfiles específicos que se
@@ -69,38 +68,51 @@ export default function Home() {
               title='Gestión de Contenidos Internos:'
               icon='gestion.svg'
             />
-          </ListContainer>
-
-          <Section>
-            <Article
-              content='Esto es una prueba'
-              image='/img/ministerio.jpg'
-              title={"Prueba"}
-            ></Article>
-            <Article
-              content='Esto es una prueba'
-              image='/img/ministerio.jpg'
-              title={"Prueba"}
-            ></Article>
-            <Article
-              content='Esto es una prueba'
-              image='/img/ministerio.jpg'
-              title={"Prueba"}
-            ></Article>
-          </Section>
-          <h4 className='text-xl font-bold'>Incribir una institución</h4>
-          <p>
-            Nuestro compromiso radica en garantizar un proceso de inscripción
-            sin complicaciones, facilitando la integración exitosa de su
-            institución educativa en nuestra plataforma.
-          </p>
-          <p>
-            Diseñamos una página que detalla paso a paso{" "}
-            <a href='/registrar-una-escuela'>cómo registrar una institución</a>,
-            también puedes ver una lista de las{" "}
-            <a href='/padron-escolar'>instituciones presentes en el padrón</a>{" "}
-            para comprobar si su escuela está presente.
-          </p>
+          </ListContainer> */}
+          <div>
+            {" "}
+            <h2 className='text-2xl font-bold'>Servicios Destacados:</h2>
+            <Section>
+              <Article
+                content='carguen y gestionen internamente sus propios recursos
+                educativos, fomentando la autonomía y la personalización del
+                contenido para los alumnos.'
+                image='/icon/gestion.svg'
+                title={"Gestión de Contenidos Internos"}
+              ></Article>
+              <Article
+                content='Facilitamos la comunicación interna entre los diversos actores
+                educativos, brindando herramientas para compartir información de
+                manera eficiente y colaborativa.'
+                image='/icon/comunicacion.svg'
+                title={"Comunicación Eficiente"}
+              ></Article>
+              <Article
+                content='MiAlumno.com facilita el proceso de registro para las
+                  escuelas, permitiéndoles crear perfiles específicos que se
+                  adaptan a las necesidades de su comunidad educativa.'
+                image='/icon/perfil.svg'
+                title={"Registro y Perfiles Específicos"}
+              ></Article>
+            </Section>
+          </div>
+          <div>
+            <h4 className='text-xl font-bold'>Incribir una institución</h4>
+            <p>
+              Nuestro compromiso radica en garantizar un proceso de inscripción
+              sin complicaciones, facilitando la integración exitosa de su
+              institución educativa en nuestra plataforma.
+            </p>
+            <p>
+              Diseñamos una página que detalla paso a paso{" "}
+              <a href='/registrar-una-escuela'>
+                cómo registrar una institución
+              </a>
+              , también puedes ver una lista de las{" "}
+              <a href='/padron-escolar'>instituciones presentes en el padrón</a>{" "}
+              para comprobar si su escuela está presente.
+            </p>
+          </div>
         </div>
       </div>
     </main>
